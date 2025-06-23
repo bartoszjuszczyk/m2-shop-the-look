b5dd3fe934a95e0028dd39f031a07857c82d0a62
Bartosz Juszczyk <b.juszczyk@bjuszczyk.pl>
2025-06-23 22:59:44 +0200

    Fixed author and copyright headers

--- Model/LookSearchResults.php
+++ Model/LookSearchResults.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 /**
  * File: LookSearchResults.php
  *
- * @author Bartosz Juszczyk <bartosz.juszczyk@ageno.pl>
- * @copyright Copyright (C) 2025 Ageno (https://ageno.pl)
+ * @author Bartosz Juszczyk <b.juszczyk@bjuszczyk.pl>
+ * @copyright Copyright (C) 2025
  */
 
 namespace Juszczyk\ShopTheLook\Model;
